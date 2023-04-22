95732315346ac01131daa400043000b1c7cbb087
AD <tab230801@gmail.com>
2023-04-22 10:18:23 +0700

    first commit

---
+++ Js.js
@@ -0,0 +1,156 @@
+//slide show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+//card group switch effect
+let activeIndex = 0;
+
+const group  = document.getElementsByClassName("card-group");
+
+const handleLoveClick = () => {
+
+    const nextIndex = activeIndex + 1 <= group.length - 1 ? activeIndex + 1 : 0;
+    
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+          nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+currentGroup.dataset.status = "after";
+
+nextGroup.dataset.status = "becoming-active-from-before";
+
+setTimeout(() => {
+    nextGroup.dataset.status = "active"
+    activeIndex = nextIndex;
+});
+}
+const handleHateClick = () => {
+
+    const nextIndex = activeIndex + 1 <= group.length - 1 ? activeIndex + 1 : 0;
+    
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+          nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+currentGroup.dataset.status = "before";
+
+nextGroup.dataset.status = "becoming-active-from-after";
+
+setTimeout(() => {
+    nextGroup.dataset.status = "active"
+    activeIndex = nextIndex;
+});
+}
+// //Text effect
+
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// let interval = null;
+
+// document.querySelector("h1").onmouseover = event => {  
+//   let iteration = 0;
+  
+//   clearInterval(interval);
+  
+//   interval = setInterval(() => {
+//     event.target.innerText = event.target.innerText
+//       .split("")
+//       .map((letter, index) => {
+//         if(index < iteration) {
+//           return event.target.dataset.value[index];
+//         }
+      
+//         return letters[Math.floor(Math.random() * 26)]
+//       })
+//       .join("");
+    
+//     if(iteration >= event.target.dataset.value.length){ 
+//       clearInterval(interval);
+//     }
+    
+//     iteration += 1 / 3;
+//   }, 30);
+// }
+// //blur effect
+
+// const blob = document.getElementById("blur");
+
+// window.onpointermove = event => { 
+//   const { clientX, clientY } = event;
+  
+//   blob.animate({
+//     left: `${clientX}px`,
+//     top: `${clientY}px`
+//   }, { duration: 3000, fill: "forwards" });
+// }
+
+
+// Galery animation
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
